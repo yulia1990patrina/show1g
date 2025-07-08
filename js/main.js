0fde56619a8d0c9fc9a230474076884ebdef3443
YliaPatrina <yulia1990patrina@gmail.com>
2025-07-08 21:31:24 +0300

    обновление сайта

--- js/main.js
+++ js/main.js
@@ -14,4 +14,177 @@ $(function(){
         $($(this).attr('href')).addClass('tabs-content--active')
     });
 
+    $('.history__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="icons/romance/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="icons/romance/arrow-right.svg" alt=""></button>',
+            responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+                dots: true
+            }
+        }
+    ]
+    });
+        $('.all-history__tabs').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="icons/romance/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="icons/romance/arrow-right.svg" alt=""></button>',
+        responsive: [
+        {
+            breakpoint: 1880,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 2,
+                arrows: false,
+                dots: true
+            }
+        }
+    ]
+    });
+    
+    $('.characters__slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="icons/romance/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="icons/romance/arrow-right.svg" alt=""></button>',
+        responsive: [
+        {
+            breakpoint: 1880,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 968,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+                dots: true
+            }
+        }
+    ]
+    });
+
+    document.querySelectorAll('.faq__item').forEach(item => {
+    const question = item.querySelector('.faq__question');
+    const answer = item.querySelector('.faq__answer');
+
+    question.addEventListener('click', () => {
+    const isOpen = item.classList.contains('active');
+    if (isOpen) {
+        answer.style.maxHeight = '0';
+        answer.style.paddingTop = '0';
+        answer.style.paddingBottom = '0';
+        item.classList.remove('active');
+    } 
+    else {
+        answer.style.maxHeight = answer.scrollHeight + '0px';
+        answer.style.paddingTop = '30px';
+        answer.style.paddingBottom = '30px';
+        item.classList.add('active');
+    }
+    });
+});
+    $('.romance-guide__slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="icons/romance/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="icons/romance/arrow-right.svg" alt=""></button>',
+            responsive: [
+        {
+            breakpoint: 1610,
+            settings: {
+                slidesToShow: 5
+            }
+        },
+        {
+            breakpoint: 1080,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+                dots: true
+            }
+        }
+    ]
+    });
+        $('.walkthrough__tabs, .walkthrough__tabs-1').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="icons/romance/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="icons/romance/arrow-right.svg" alt=""></button>',
+        responsive: [
+        {
+            breakpoint: 1610,
+            settings: {
+                slidesToShow: 6
+            }
+        },
+        {
+            breakpoint: 1420,
+            settings: {
+                slidesToShow: 5
+            }
+        },
+        {
+            breakpoint: 1080,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 3,
+                arrows: false,
+                dots: true
+            }
+        }
+    ]
+    });
+    document.querySelectorAll('.toggle-submenu').forEach(btn => {
+    btn.addEventListener('click', () => {
+    btn.parentElement.classList.toggle('open');
+    });
+    });
 });
