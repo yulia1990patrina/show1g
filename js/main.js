e2a01670cf2eac4132ffe6e0e72a17535de9082d
YliaPatrina <yulia1990patrina@gmail.com>
2025-08-09 13:31:39 +0300

    добавила изменения

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(function(){
     $('.banner-section__slider').slick({
     dots: true,
-    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"></button>',
+    arrows: false,
     });
     
     $('.tab').on('click', function(e){
